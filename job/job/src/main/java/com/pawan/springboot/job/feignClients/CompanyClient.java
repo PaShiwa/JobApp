9ad9401183b1497812f767b7f04868354da05379
Pawan Shiwakoti <pawanshiwakoti45@gmail.com>
2024-12-25 23:51:19 -0700

    Dockerized the JobApp microservices with docker compose filed placed in company microservices,application-docker.properties has been used for setting spring profiles.

--- job/job/src/main/java/com/pawan/springboot/job/feignClients/CompanyClient.java
+++ job/job/src/main/java/com/pawan/springboot/job/feignClients/CompanyClient.java
@@ -4,9 +4,8 @@ import com.pawan.springboot.job.ext.Company;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name="COMPANY-SERVICE")
+@FeignClient(name="COMPANY-SERVICE", url = "${company-service.url}")
 public interface CompanyClient {
     @GetMapping("/companies/{id}")
     Company getCompany(@PathVariable("id") Long companyId);
--- review/review/src/test/java/com/pawan/springboot/ReviewApplicationTests.java
+++ review/review/src/test/java/com/pawan/springboot/ReviewApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReveiwApplicationTests {
+class ReviewApplicationTests {
 
     @Test
     void contextLoads() {
